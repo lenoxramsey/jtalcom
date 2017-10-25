ac5dd5af3e65a03048574255b6b574bcf2ce61b6
Lenox Ramsey Jr <lenoxramseyjr@outlook.com>
2017-10-25 18:18:29 +0000

    Added jquery, footer, topbar and user authentication modal

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :recruiters, path: 'hires', controllers: { sessions: "recruiters/sessions" }
   devise_for :users, path: 'members', controllers: { sessions: "users/sessions" }
   
-  root to: "home#index"
+  resources :jobs
+  
+  root to: "home#hero"
 end
