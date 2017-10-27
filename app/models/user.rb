101a400007317726c73f3c944acf6951ba0a6154
Lenox Ramsey Jr <lenoxramseyjr@outlook.com>
2017-10-27 04:56:01 +0000

    Added Google Omniauth Modal

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :bookmarks
   has_many :jobs, through: :bookmarks
+  
+  def self.from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(email: data['email']).first
+  
+      # Uncomment the section below if you want users to be created if they don't exist
+      unless user
+        user = User.create(email: data['email'],
+        password: Devise.friendly_token[0,20])
+      end
+      user
+  end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :recruiters, path: 'hires', controllers: { sessions: "recruiters/sessions" }
-  devise_for :users, path: 'members', controllers: { sessions: "users/sessions", omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, path: 'members', controllers: { sessions: "users/sessions", omniauth_callbacks: 'users/omniauth_callbacks', passwords: "users/passwords" }
   
   resources :jobs
   
